b7db49ac3be65f07cd29d5b3dcd2bcd866929731
Alexander Berezhnev <freelancerbav@gmail.com>
2023-04-18 09:47:51 +0300

    added the component RImputArray (imputArray with redux)

--- client/src/components/Zwift/UI/FormEditEventGroup/SubGroup.jsx
+++ client/src/components/Zwift/UI/FormEditEventGroup/SubGroup.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { jerseys, routes, worlds } from '../../../../asset/zwift/lib/esm/zwift-lib';
 import RTextarea from '../../../UI/ReduxUI/RTextarea/RTextarea';
 import RSelectObject from '../../../UI/ReduxUI/RSelectObject/RSelectObject';
+import RInputArray from '../../../UI/ReduxUI/RInputArray/RInputArray';
 
 import styles from './FormEditEventGroup.module.css';
 
@@ -33,12 +34,57 @@ function SubGroup({ subGroup, index }) {
               .sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase(), 'en'))}
             indexArray={index}
           />
-
+          <RInputArray
+            label={'Количество кругов'}
+            value={subGroup.laps}
+            property={'laps'}
+            type={'number'}
+            indexArray={index}
+          />
+          <RInputArray
+            label={'Время старта (московское время -3ч)'}
+            value={subGroup.eventSubgroupStart}
+            property={'eventSubgroupStart'}
+            type={'text'}
+            indexArray={index}
+          />
+          <RInputArray
+            label={'Название для группы'}
+            value={subGroup.name}
+            property={'name'}
+            type={'text'}
+            indexArray={index}
+          />
           <RTextarea
             label={'Описание для группы'}
             value={subGroup.description}
             property={'description'}
             type={'text'}
+            indexArray={index}
+          />
+          <RSelectObject
+            label={'Джерси заезда'}
+            value={subGroup.jerseyHash}
+            property={'jerseyHash'}
+            options={jerseys.sort((a, b) =>
+              a.name.toLowerCase().localeCompare(b.name.toLowerCase(), 'en')
+            )}
+            indexArray={index}
+          />
+          <RInputArray
+            label={'Номер места старта'}
+            value={subGroup.startLocation}
+            property={'startLocation'}
+            type={'number'}
+            indexArray={index}
+          />
+          <RInputArray
+            label={'Номер пакета правил'}
+            value={subGroup.rulesId}
+            property={'rulesId'}
+            type={'number'}
+            indexArray={index}
+            disabled={true}
           />
         </div>
       ) : undefined}
