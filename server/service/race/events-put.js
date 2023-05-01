2d900a6c2f331b3955ed90eeb152cecef2a91aef
Alexander Berezhnev <freelancerbav@gmail.com>
2023-05-01 10:36:45 +0300

    logging requests for updating schedule data

--- server/service/race/events-put.js
+++ server/service/race/events-put.js
@@ -6,7 +6,10 @@ export async function putEventService(eventId) {
   try {
     const { additionalParams } = await deleteEventService(eventId);
     const event = await getEventService(eventId);
+    console.log(new Date(), '4.получение обновленных данных эвента');
     await postEventService({ ...event, ...additionalParams });
+    console.log(new Date(), '5.обновление данных эвента в БД');
+    console.log('======================================================');
     return { message: `Обновлены данные ${event.name}` };
   } catch (error) {
     throw error;
