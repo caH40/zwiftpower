0016789d1b53252a1a88c6a959761acee75f9555
Alexander Berezhnev <freelancerbav@gmail.com>
2023-05-05 19:58:23 +0300

    column names are created using map

---
+++ client/src/components/Tables/TableSchedule/column-titles.js
@@ -0,0 +1,13 @@
+export const scheduleListColumns = [
+  { name: 'Дата старта', id: 0 },
+  { name: 'Название', id: 1 },
+  { name: 'Организатор', id: 2 },
+  { name: 'Тип заезда', id: 3 },
+  { name: 'Регистрация', id: 4 },
+  { name: 'Карта', id: 5 },
+  { name: 'Маршрут', id: 6 },
+  { name: 'Круги', id: 7 },
+  { name: 'Расстояние', id: 8 },
+  { name: 'Подъем', id: 9 },
+  { name: 'Прод.', id: 10 },
+];
---
+++ client/src/components/Tables/TableSignedRiders/column-titles.js
@@ -0,0 +1,11 @@
+export const signedRidersColumns = [
+  { name: '#', id: 0 },
+  { name: 'Кат', id: 1 },
+  { name: 'Райдер', id: 2 },
+  { name: 'Команда', id: 3 },
+  { name: 'Вес', id: 4 },
+  { name: 'Рост', id: 5 },
+  { name: 'Вз.кат', id: 6 },
+  { name: 'Пол', id: 7 },
+  { name: 'zp.com', id: 8 },
+];
