d3e927c4aa5387845ff54875499232c44ef0a05d
Alexander Berezhnev <freelancerbav@gmail.com>
2023-05-10 08:02:25 +0300

    added icons categories to tables results and schedules

--- client/src/components/CategoriesBox/CategoriesBox.jsx
+++ client/src/components/CategoriesBox/CategoriesBox.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import cn from 'classnames';
 
+import { addClasses as cns } from '../../utils/additional-classes';
 import { gapStart } from '../../utils/event';
 import CategoryBox from '../CategoryBox/CategoryBox';
 
 import styles from './CategoriesBox.module.css';
 
-function CategoriesBox({ event }) {
+function CategoriesBox({ event, needGaps, addCls }) {
   const gaps = gapStart(event);
   return (
-    <div className={styles.block}>
-      {event.eventSubgroups.map((group) => (
-        <div className={styles.box} key={group.id}>
-          <CategoryBox label={group.subgroupLabel} quantityRiders={group.totalEntrantCount} />
-          {gaps[group.subgroupLabel] === 0 ? null : (
-            <span>+{gaps[group.subgroupLabel]}мин</span>
-          )}
-        </div>
-      ))}
+    <div className={cn(styles.block, cns(styles, addCls))}>
+      {event.eventSubgroups.map((group) => {
+        const gap =
+          gaps[group.subgroupLabel] === 0 ? null : <span>+{gaps[group.subgroupLabel]}мин</span>;
+
+        return (
+          <div className={styles.box} key={group.id}>
+            <CategoryBox label={group.subgroupLabel} quantityRiders={group.totalEntrantCount} />
+            {needGaps && gap}
+          </div>
+        );
+      })}
     </div>
   );
 }
--- client/src/components/Tables/TableResults/column-titles.js
+++ client/src/components/Tables/TableResults/column-titles.js
@@ -3,7 +3,7 @@ export const resultsColumns = [
   { name: 'Название', id: 1 },
   { name: 'Организатор', id: 2 },
   { name: 'Тип заезда', id: 3 },
-  { name: 'Финишировало', id: 4 },
+  { name: 'Финиш', id: 4 },
   { name: 'Карта', id: 5 },
   { name: 'Маршрут', id: 6 },
   { name: 'Круги', id: 7 },
