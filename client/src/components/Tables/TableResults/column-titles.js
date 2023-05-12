43c202d16da3a33c10498b26aeef29131a9a51e0
Alexander Berezhnev <freelancerbav@gmail.com>
2023-05-12 22:49:03 +0300

    added mobile resultsList

--- client/src/components/Tables/TableResults/column-titles.js
+++ client/src/components/Tables/TableResults/column-titles.js
@@ -1,4 +1,4 @@
-export const resultsColumns = [
+export const resultsColumnsFull = [
   { name: 'Дата', id: 0 },
   { name: 'Название', id: 1 },
   { name: '', id: 2 },
@@ -7,7 +7,25 @@ export const resultsColumns = [
   { name: 'Карта', id: 5 },
   { name: 'Маршрут', id: 6 },
   { name: 'Круги', id: 7 },
-  { name: 'Расстояние', id: 8 },
+  { name: 'Расст.', id: 8 },
   { name: 'Подъем', id: 9 },
   { name: 'Прод.', id: 10 },
 ];
+export const resultsColumnsLg = [
+  { name: 'Дата', id: 0 },
+  { name: 'Название', id: 1 },
+  { name: 'Финиш', id: 4 },
+  { name: 'Карта', id: 5 },
+  { name: 'Маршрут', id: 6 },
+];
+export const resultsColumnsSm = [
+  { name: 'Дата', id: 0 },
+  { name: 'Название', id: 1 },
+  { name: 'Финиш', id: 4 },
+];
+
+export const resultsColumns = (isScreenLg, isScreenSm) => {
+  if (!isScreenLg && isScreenSm) return resultsColumnsLg;
+  if (!isScreenSm) return resultsColumnsSm;
+  return resultsColumnsFull;
+};
