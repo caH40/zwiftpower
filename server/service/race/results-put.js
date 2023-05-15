8880c6dc5220af63f2a54575713edccac96fe95d
Alexander Berezhnev <freelancerbav@gmail.com>
2023-05-15 22:36:00 +0300

    added CP update when automatically updating results

--- server/service/race/results-put.js
+++ server/service/race/results-put.js
@@ -1,27 +1,12 @@
 import { ZwiftEvent } from '../../Model/ZwiftEvent.js';
-import { handlerProtocol } from '../protocol/handler.js';
-import { addCriticalPowers } from './criticalpower.js';
-
-import { getResults } from './results.js';
+import { updateResultsEvent } from '../updates/results-events.js';
 
 // ручное обновление результатов по запросу модератора
 export async function putResultsService(eventId) {
   try {
-    const eventsDB = await ZwiftEvent.findOne({ id: eventId }).populate('eventSubgroups');
-
-    const resultsTotal = [];
-    for (const subgroup of eventsDB.eventSubgroups) {
-      const resultsSubgroup = await getResults(
-        { subgroup_id: subgroup._id, subgroupId: subgroup.id },
-        subgroup.subgroupLabel
-      );
-      resultsTotal.push(...resultsSubgroup);
-    }
-
-    const resultsWithCP = await addCriticalPowers(resultsTotal);
-
-    await handlerProtocol(eventsDB._id, resultsWithCP, eventsDB.typeRaceCustom);
-    return { message: `Обновлены результаты заезда "${eventsDB.name}"` };
+    const eventDB = await ZwiftEvent.findOne({ id: eventId }).populate('eventSubgroups');
+    await updateResultsEvent(eventDB);
+    return { message: `Обновлены результаты заезда "${eventDB.name}"` };
   } catch (error) {
     throw error;
   }
--- server/service/updates/results-events.js
+++ server/service/updates/results-events.js
@@ -1,5 +1,6 @@
 import { ZwiftEvent } from '../../Model/ZwiftEvent.js';
 import { handlerProtocol } from '../protocol/handler.js';
+import { addCriticalPowers } from '../race/criticalpower.js';
 import { getResults } from '../race/results.js';
 
 import { checkDurationUpdating } from './results-check.js';
@@ -13,18 +14,24 @@ export async function updateResults() {
 
     for (const event of eventsDB) {
       await checkDurationUpdating(event); // обновление результатов заезда заканчивается после 2х часов после старта
-
-      const resultsTotal = [];
-      for (const subgroup of event.eventSubgroups) {
-        const resultsSubgroup = await getResults(
-          { subgroup_id: subgroup._id, subgroupId: subgroup.id },
-          subgroup.subgroupLabel
-        );
-        resultsTotal.push(...resultsSubgroup);
-      }
-      await handlerProtocol(event._id, resultsTotal, event.typeRaceCustom);
+      await updateResultsEvent(event);
     }
   } catch (error) {
     console.error(error);
   }
+}
+
+export async function updateResultsEvent(event) {
+  const resultsTotal = [];
+  for (const subgroup of event.eventSubgroups) {
+    const resultsSubgroup = await getResults(
+      { subgroup_id: subgroup._id, subgroupId: subgroup.id },
+      subgroup.subgroupLabel
+    );
+    resultsTotal.push(...resultsSubgroup);
+  }
+
+  const resultsWithCP = await addCriticalPowers(resultsTotal);
+
+  await handlerProtocol(event._id, resultsWithCP, event.typeRaceCustom);
 }
