f5db948012085854a0e89f4df1f059115b01eb63
Alexander Berezhnev <freelancerbav@gmail.com>
2023-06-14 09:22:35 +0300

    added the model CriricalPower

---
+++ server/Model/PowerCurve.js
@@ -0,0 +1,24 @@
+import pkg from 'mongoose';
+
+const { Schema, model } = pkg;
+
+const powerCurveSchema = new Schema({
+  zwiftId: { type: Number, required: true, unique: true },
+  dateLastRace: { type: Number },
+  pointsWatts: [
+    {
+      duration: { type: Number, default: null },
+      value: { type: Number, default: null },
+      date: { type: Number, default: null },
+    },
+  ],
+  pointsWattsPerKg: [
+    {
+      duration: { type: Number, default: null },
+      value: { type: Number, default: null },
+      date: { type: Number, default: null },
+    },
+  ],
+});
+
+export const PowerCurve = model('PowerCurve', powerCurveSchema);
