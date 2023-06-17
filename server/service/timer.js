d1419f8309cf7af9a7c53894d3dfeeb322f6899b
Alexander Berezhnev <freelancerbav@gmail.com>
2023-06-17 13:43:36 +0300

    nightly launch of power rider profile updates

--- server/service/timer.js
+++ server/service/timer.js
@@ -1,7 +1,10 @@
 import { controlConfirmEmail } from './authentication/control-confirm-email.js';
 import { controlNewPasswords } from './authentication/control-newpassword.js';
+import { updateAllPowerCurve } from './updates/power-curve.js';
 import { updateResults } from './updates/results-events.js';
 import { updateScheduleEvents, updateStartInfo } from './updates/schedule-events.js';
+import { CronJob } from 'cron';
+import { updateAccessToken } from './zwift/token.js';
 
 export async function timers() {
   const millisecondsInDay = 24 * 60 * 60 * 1000;
@@ -32,4 +35,17 @@ export async function timers() {
       console.log(error);
     }
   }, millisecondsIn12Minutes);
+
+  // запуск обновления фитфалов мощности в 2 часа каждую ночь
+  const job = new CronJob(
+    '0 0 2 * * *',
+    async function () {
+      console.log(new Date().toLocaleString(), 'Обновление токенов и фитфайлов мощности');
+      await updateAccessToken();
+      await updateAllPowerCurve();
+    },
+    null,
+    true,
+    'Europe/Moscow'
+  );
 }
