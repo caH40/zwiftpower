c054dd42baf29949364706c706196549d34ebbe1
Alexander Berezhnev <freelancerbav@gmail.com>
2023-07-16 23:04:38 +0300

    2.5.1-FAQ page

---
+++ client/src/components/FaqCategory/FaqCategory.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import IconCategoryEnforced from '../icons/IconCategoryEnforced';
+import TableCategory from '../Tables/TableCategory/TableCategory';
+
+import styles from './FaqCategory.module.css';
+
+function FaqCategory() {
+  return (
+    <div className={styles.block}>
+      <h3 className={styles.title}>Отображение категорий на основе zFTP, zMAP</h3>
+      <div className={styles.box}>
+        <p className={styles.text}>
+          Приблизительная оценка категории согласно информации, которая есть в открытом доступе
+          на сайте Звифта:{' '}
+          <a
+            href="https://support.zwift.com/en_us/category-enforcement-faq-rJ5CQrpvc"
+            className={styles.link}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Category Enforcement FAQ
+          </a>
+        </p>
+        <div className={styles.icon}>
+          <IconCategoryEnforced squareSize={50} />
+        </div>
+      </div>
+      <TableCategory />
+      <div className={styles.box}>
+        <p className={styles.text}>
+          <strong>zMAP</strong> - Estimate of your Maximal Aerobic Power, i.e the power at which
+          you achieve peak oxygen consumption in a short duration effort 4-6 min.
+        </p>
+      </div>
+      <div className={styles.box}>
+        <p className={styles.text}>
+          <strong>zFTP</strong> - Estimate of your current Functional Threshold Power (FTP), the
+          power you can sustain for a long period of time, i.e. {'>'} 40 min.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default FaqCategory;
--- client/src/components/MainInfo/MainInfo.jsx
+++ client/src/components/MainInfo/MainInfo.jsx
@@ -4,57 +4,55 @@ import styles from './MainInfo.module.css';
 
 function MainInfo() {
   return (
-    <div>
-      <div className={styles.block}>
-        <h3 className={styles.title}>Для чего ZP.ru?</h3>
-        <div className={styles.block__text}>
-          Сайт создан для корректного формирования протоколов заездов, основанных на правилах,
-          созданных организаторами заездов в Звифт.
-        </div>
-        <h3 className={styles.title}>Какие типы заездов?</h3>
-        <div className={styles.block__text}>
-          На данный момент поддерживаются следующие типы заездов, проводимые командой{' '}
-          <strong>"KOM-on"</strong>:
-          <ul className={styles.list}>
-            <li className={styles.item}>
-              {' '}
-              <span className={styles.text}>догонялки</span>
-            </li>
-            <li className={styles.item}>
-              {' '}
-              <span className={styles.text}>для новичков</span>
-            </li>
-            <li className={styles.item}>
-              {' '}
-              <span className={styles.text}>классический</span>
-            </li>
-            <li className={styles.item}>
-              {' '}
-              <span className={styles.text}>классический без групп</span>
-            </li>
-            <li className={styles.item}>
-              <span className={styles.text}>объём</span>
-            </li>
-          </ul>
-        </div>
-        <h3 className={styles.title}>Улучшение сайта?</h3>
-        <div className={styles.block__text}>
-          <p className={styles.p}>
-            В ближайшее время будет создана форма обратной связи для Ваших пожеланий в улучшении
-            сайта и добавления новых фитчей для zwiftpower.ru.
-          </p>
-          <p>
-            Для более оперативной обратной связи создана группа{' '}
-            <a
-              className={styles.link}
-              target="_blank"
-              rel="noreferrer"
-              href="https://t.me/zwiftpower"
-            >
-              https://t.me/zwiftpower
-            </a>
-          </p>
-        </div>
+    <div className={styles.block}>
+      <h3 className={styles.title}>Для чего ZP.ru?</h3>
+      <div className={styles.block__text}>
+        Сайт создан для корректного формирования протоколов заездов, основанных на правилах,
+        созданных организаторами заездов в Звифт.
+      </div>
+      <h3 className={styles.title}>Какие типы заездов?</h3>
+      <div className={styles.block__text}>
+        На данный момент поддерживаются следующие типы заездов, проводимые командой{' '}
+        <strong>"KOM-on"</strong>:
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            {' '}
+            <span className={styles.text}>догонялки</span>
+          </li>
+          <li className={styles.item}>
+            {' '}
+            <span className={styles.text}>для новичков</span>
+          </li>
+          <li className={styles.item}>
+            {' '}
+            <span className={styles.text}>классический</span>
+          </li>
+          <li className={styles.item}>
+            {' '}
+            <span className={styles.text}>классический без групп</span>
+          </li>
+          <li className={styles.item}>
+            <span className={styles.text}>объём</span>
+          </li>
+        </ul>
+      </div>
+      <h3 className={styles.title}>Улучшение сайта?</h3>
+      <div className={styles.block__text}>
+        <p className={styles.p}>
+          В ближайшее время будет создана форма обратной связи для Ваших пожеланий в улучшении
+          сайта и добавления новых фитчей для zwiftpower.ru.
+        </p>
+        <p>
+          Для более оперативной обратной связи создана группа{' '}
+          <a
+            className={styles.link}
+            target="_blank"
+            rel="noreferrer"
+            href="https://t.me/zwiftpower"
+          >
+            https://t.me/zwiftpower
+          </a>
+        </p>
       </div>
     </div>
   );
