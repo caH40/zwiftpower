38f2417b5e944569da78e7ec3af600674863e7e0
Alexander Berezhnev <freelancerbav@gmail.com>
2023-07-17 23:01:01 +0300

    2.7.1 Scroll to top

--- client/src/index.js
+++ client/src/index.js
@@ -8,11 +8,13 @@ import store from './redux/store/store';
 
 import App from './App';
 import './css/index.css';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
+      <ScrollToTop />
       <App />
     </Provider>
   </BrowserRouter>
