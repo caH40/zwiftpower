16b9a99b6f9e45ddd1a5423bbfc1acaf6e4309df
Alexander Berezhnev <freelancerbav@gmail.com>
2023-07-23 11:46:07 +0300

    checking the portrait orientation of the screen

--- client/src/Pages/Profile/ProfilePower.jsx
+++ client/src/Pages/Profile/ProfilePower.jsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-function ProfilePower() {
-  return <h2>В разработке...</h2>;
+import { Line } from 'react-chartjs-2';
+
+import { fetchUserResults } from '../../redux/features/api/userResultsSlice';
+import useTitle from '../../hook/useTitle';
+import useBackground from '../../hook/useBackground';
+import useChartPower from '../../hook/useChartPower';
+import useScreenOrientation from '../../hook/useScreenOrientation';
+import SimpleCheckbox from '../../components/UI/SimpleCheckbox/SimpleCheckbox';
+
+import styles from './ProfilePower.module.css';
+
+function ProfileWeight() {
+  const [formShowCharts, setFormShowCharts] = useState({
+    showChart90Days: true,
+    showChartLastRide: true,
+  });
+  const { zwiftId } = useParams();
+  const userAuth = useSelector((state) => state.checkAuth.value);
+
+  const { isPortrait } = useScreenOrientation();
+
+  const { data, options } = useChartPower(isPortrait, formShowCharts);
+  useTitle('Профиль мощности');
+  useBackground(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const currentZwiftId = zwiftId === 'me' ? userAuth.user.zwiftId : zwiftId;
+    if (!currentZwiftId) return;
+    dispatch(fetchUserResults({ zwiftId: currentZwiftId }));
+  }, [dispatch, zwiftId, userAuth]);
+
+  return (
+    <section>
+      <div className={styles.block}>
+        <div className={styles.chart}>
+          <Line options={options} data={data} />
+        </div>
+        <div className={styles.box__checkbox}>
+          <SimpleCheckbox
+            state={formShowCharts}
+            setState={setFormShowCharts}
+            property={'showChart90Days'}
+            title={'90 дней'}
+          />
+          <SimpleCheckbox
+            state={formShowCharts}
+            setState={setFormShowCharts}
+            property={'showChartLastRide'}
+            title={'Последний заезд'}
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default ProfilePower;
+export default ProfileWeight;
---
+++ client/src/hook/useScreenOrientation.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+const getOrientation = () => window.screen?.orientation?.type;
+
+const useScreenOrientation = () => {
+  const [orientation, setOrientation] = useState(getOrientation());
+
+  const updateOrientation = (event) => {
+    setOrientation(getOrientation());
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', updateOrientation);
+    return () => {
+      window.removeEventListener('resize', updateOrientation);
+    };
+  }, []);
+
+  return { isPortrait: orientation.includes('portrait') };
+};
+
+export default useScreenOrientation;
