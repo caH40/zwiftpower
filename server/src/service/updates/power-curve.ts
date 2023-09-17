b6cc4ba222b5c0c1ce613917b46db50599569946
Alexander Berezhnev <freelancerbav@gmail.com>
2023-09-17 10:30:47 +0300

    v2.14.7-fixed the bug when updating CriticalPower

--- server/src/service/updates/power-curve.ts
+++ server/src/service/updates/power-curve.ts
@@ -1,6 +1,6 @@
 import { PowerCurve } from '../../Model/PowerCurve.js';
 import { PowerCurveSchema } from '../../types/model.interface.js';
-// import { createFitFiles } from '../zwift/fitfiles/fitfiles.js';
+import { createFitFiles } from '../zwift/fitfiles/fitfiles.js';
 
 // types
 import { updatePowerCurve } from '../zwift/power_curve/power-curve-update.js';
@@ -8,9 +8,9 @@ import { updatePowerCurve } from '../zwift/power_curve/power-curve-update.js';
 export async function updateAllPowerCurve() {
   try {
     const powerCurvesDB: PowerCurveSchema[] = await PowerCurve.find();
-    // for (const powerCurve of powerCurvesDB) {
-    //   await createFitFiles(powerCurve.zwiftId);
-    // }
+    for (const powerCurve of powerCurvesDB) {
+      await createFitFiles(powerCurve.zwiftId);
+    }
     for (const powerCurve of powerCurvesDB) {
       await updatePowerCurve(powerCurve.zwiftId);
     }
