ea60b7f03c949c0cbfd7d17d707f9ee488ba475e
Alexander Berezhnev <freelancerbav@gmail.com>
2023-10-13 11:49:53 +0300

    2.26.13 deleting the data of the Main Swift profile from the results of Events shown by the Additional Swift Profile

--- server/src/service/user.ts
+++ server/src/service/user.ts
@@ -1,5 +1,5 @@
 import { User } from '../Model/User.js';
-import { addMainProfileZwift } from './result-profile.js';
+import { addMainProfileZwift, deleteMainProfileZwift } from './result-profile.js';
 import { getZwiftRiderService } from './zwift/rider.js';
 
 /**
@@ -62,6 +62,10 @@ export async function deleteUserZwiftIdService(userId: string, zwiftId: number) 
   if (!userWithZwiftId || userWithZwiftId.zwiftIdAdditional.includes(zwiftId)) {
     throw new Error(`Дополнительный ZwiftId ${zwiftId} не был удалён из профиля`);
   }
+
+  // удаление данных Основного профиля Звифт райдера из результатов,
+  // показанных дополнительным профилем с zwiftId
+  await deleteMainProfileZwift(zwiftId);
 
   return {
     message: `Дополнительный ZwiftId ${zwiftId} удалён из профиля`,
