994aeed5550878e945ba914597c2299609d8a72a
Alexander Berezhnev <freelancerbav@gmail.com>
2023-10-15 11:41:33 +0300

    v2.26.23 ban on updating the results of an Event that is older than a week

--- server/src/assets/date.ts
+++ server/src/assets/date.ts
@@ -1,13 +1,15 @@
 // переменные для времени (даты)
 
-const millisecondsInMinute = 60 * 1000;
+export const millisecondsInMinute = 60 * 1000;
 
 export const millisecondsInDay = 24 * 60 * millisecondsInMinute;
 export const millisecondsIn23Minutes = 23 * millisecondsInMinute;
 export const millisecondsIn10Minutes = 10 * millisecondsInMinute;
 export const millisecondsIn12Minutes = 12 * millisecondsInMinute;
 export const millisecondsInHour = 60 * millisecondsInMinute;
-export const millisecondsIn90Days = 90 * 24 * 60 * millisecondsInMinute;
+
+export const millisecondsInWeekDays = 7 * millisecondsInDay;
+export const millisecondsIn90Days = 90 * millisecondsInDay;
 
 /**
  * Дата 90 дней назад
--- server/src/service/updates/results-events.ts
+++ server/src/service/updates/results-events.ts
@@ -4,6 +4,7 @@ import { handlerProtocol } from '../protocol/handler.js';
 import { addCriticalPowers } from '../race/criticalpower.js';
 import { getResults } from '../zwift/results.js';
 import { checkDurationUpdating } from './results-check.js';
+import { millisecondsInWeekDays } from '../../assets/date.js';
 
 // types
 import { EventWithSubgroup, ResultEventAdditional } from '../../types/types.interface.js';
@@ -30,42 +31,45 @@ export async function updateResults() {
  * Ручное обновление результатов по запросу модератора
  */
 export async function updateResultsEvent(event: EventWithSubgroup) {
-  try {
-    const resultsTotal = [] as ResultEventAdditional[];
+  const resultsTotal = [] as ResultEventAdditional[];
 
-    for (const subgroup of event.eventSubgroups) {
-      const subgroupObj = { subgroup_id: subgroup._id, subgroupId: subgroup.id };
-
-      // получение результатов заезда из Звифта
-      const resultsSubgroup = await getResults({
-        subgroupObj,
-        subgroupLabel: subgroup.subgroupLabel,
-      });
-      resultsTotal.push(...resultsSubgroup);
-    }
-
-    // добавление CP в результаты райдеров
-    const nameAndDate = { name: event.name, eventStart: new Date(event.eventStart).getTime() };
-    const resultsWithCP = await addCriticalPowers(resultsTotal, nameAndDate);
-
-    if (!event._id) {
-      throw new Error(`Не найден event._id ${event._id}`);
-    }
-    if (!resultsWithCP) {
-      throw new Error(`Не найден resultsWithCP ${resultsWithCP}`);
-    }
-
-    // параметры для функции handlerProtocol
-    const handlerProtocolArg = {
-      eventId: event._id,
-      results: resultsWithCP,
-      typeRaceCustom: event.typeRaceCustom,
-    };
-
-    // обработка результатов, согласно типа (typeRaceCustom) Эвента
-    // и сохранение в БД
-    await handlerProtocol(handlerProtocolArg);
-  } catch (error) {
-    errorHandler(error);
+  // !!! если заезд был больше недели, то не обновлять
+  const eventStart = new Date(event.eventStart).getTime();
+  const notRequiredUpdate = Date.now() - eventStart > millisecondsInWeekDays;
+  if (notRequiredUpdate) {
+    throw new Error('Запрет на обновление результатов Эвента который старше недели!');
   }
+
+  for (const subgroup of event.eventSubgroups) {
+    const subgroupObj = { subgroup_id: subgroup._id, subgroupId: subgroup.id };
+
+    // получение результатов заезда из Звифта
+    const resultsSubgroup = await getResults({
+      subgroupObj,
+      subgroupLabel: subgroup.subgroupLabel,
+    });
+    resultsTotal.push(...resultsSubgroup);
+  }
+
+  // добавление CP в результаты райдеров
+  const nameAndDate = { name: event.name, eventStart: new Date(event.eventStart).getTime() };
+  const resultsWithCP = await addCriticalPowers(resultsTotal, nameAndDate);
+
+  if (!event._id) {
+    throw new Error(`Не найден event._id ${event._id}`);
+  }
+  if (!resultsWithCP) {
+    throw new Error(`Не найден resultsWithCP ${resultsWithCP}`);
+  }
+
+  // параметры для функции handlerProtocol
+  const handlerProtocolArg = {
+    eventId: event._id,
+    results: resultsWithCP,
+    typeRaceCustom: event.typeRaceCustom,
+  };
+
+  // обработка результатов, согласно типа (typeRaceCustom) Эвента
+  // и сохранение в БД
+  await handlerProtocol(handlerProtocolArg);
 }
