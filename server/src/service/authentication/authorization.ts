769e868d24ee5ee778a74eff04e6790aaee7da4a
Alexander Berezhnev <freelancerbav@gmail.com>
2023-10-28 22:23:50 +0300

    v2.31.3 fixing a bug when sending a message about an incorrect login or password during authorization

--- server/src/service/authentication/authorization.ts
+++ server/src/service/authentication/authorization.ts
@@ -2,56 +2,50 @@ import bcrypt from 'bcrypt';
 
 import { User } from '../../Model/User.js';
 import { generateToken, removeToken, saveToken } from './token.js';
-import { errorHandler } from '../../errors/error.js';
 
 export async function authorizationService(
   username: string,
   password: string,
   refreshToken: string
 ) {
-  try {
-    const userDB = await User.findOne({ username });
+  const userDB = await User.findOne({ username });
 
-    const wrongAuth = { message: `Неверный Логин или Пароль`, status: 'wrong' };
-    if (!userDB) {
-      throw wrongAuth;
-    }
+  if (!userDB) {
+    throw new Error(`Неверный Логин или Пароль`);
+  }
 
-    const isValidPassword = await bcrypt.compare(password, userDB.password);
-    if (!isValidPassword) {
-      throw wrongAuth;
-    }
+  const isValidPassword = await bcrypt.compare(password, userDB.password);
+  if (!isValidPassword) {
+    throw new Error(`Неверный Логин или Пароль`);
+  }
 
-    await removeToken(refreshToken);
+  await removeToken(refreshToken);
 
-    const tokens = await generateToken({
+  const tokens = await generateToken({
+    username,
+    email: userDB.email,
+    id: userDB._id,
+    zwiftId: userDB.zwiftId,
+    role: userDB.role,
+  });
+
+  if (tokens) {
+    await saveToken(userDB._id, tokens.refreshToken);
+  } else {
+    throw new Error('Ошибка при получении токенов');
+  }
+
+  const message = 'Авторизация прошла успешно';
+  return {
+    ...tokens,
+    message,
+    user: {
       username,
       email: userDB.email,
       id: userDB._id,
-      zwiftId: userDB.zwiftId,
       role: userDB.role,
-    });
-
-    if (tokens) {
-      await saveToken(userDB._id, tokens.refreshToken);
-    } else {
-      throw new Error('Ошибка при получении токенов');
-    }
-
-    const message = 'Авторизация прошла успешно';
-    return {
-      ...tokens,
-      message,
-      user: {
-        username,
-        email: userDB.email,
-        id: userDB._id,
-        role: userDB.role,
-        photoProfile: userDB.photoProfile,
-        zwiftId: userDB.zwiftId,
-      },
-    };
-  } catch (error) {
-    errorHandler(error);
-  }
+      photoProfile: userDB.photoProfile,
+      zwiftId: userDB.zwiftId,
+    },
+  };
 }
