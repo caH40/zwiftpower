72d66c1bb88d7f1e103764e34b983a8e7c0670f0
Alexander Berezhnev <freelancerbav@gmail.com>
2023-10-31 09:27:10 +0300

    v2.33.3 adding links to route descriptions on third-party resources

--- client/src/components/LinksRoute/LinksRoute.jsx
+++ client/src/components/LinksRoute/LinksRoute.jsx
@@ -1,31 +1,39 @@
-import React from 'react';
-
-import { routeLinks } from '../../utils/event';
+import { getLinksRouteDescription } from '../../utils/event';
 
 import styles from './LinksRoute.module.css';
 
 function LinksRoute({ routeId }) {
-  const { whatsOnZwiftUrl, zwiftInsiderUrl, stravaSegmentUrl } = routeLinks(routeId);
+  const { linkZwifterbikes, linkZwiftinsider, linkWhatsonzwift } =
+    getLinksRouteDescription(routeId);
   return (
     <div className={styles.block}>
-      {zwiftInsiderUrl && (
+      {linkZwiftinsider && (
         <p className={styles.route__p}>
-          <a className={styles.link} href={zwiftInsiderUrl} target="_blank" rel="noreferrer">
+          <a className={styles.link} href={linkZwiftinsider} target="_blank" rel="noreferrer">
             Описание маршрута на zwiftInsider.com
           </a>
         </p>
       )}
-      {whatsOnZwiftUrl && (
+      {linkWhatsonzwift && (
         <p className={styles.route__p}>
-          <a className={styles.link} href={whatsOnZwiftUrl} target="_blank" rel="noreferrer">
+          <a className={styles.link} href={linkWhatsonzwift} target="_blank" rel="noreferrer">
             Описание маршрута на whatsonzwift.com
           </a>
         </p>
       )}
+      {/* 
       {stravaSegmentUrl && (
         <p className={styles.route__p}>
           <a className={styles.link} href={stravaSegmentUrl} target="_blank" rel="noreferrer">
             Сегмент на strava.com
+          </a>
+        </p>
+      )}{' '}
+     */}
+      {linkZwifterbikes && (
+        <p className={styles.route__p}>
+          <a className={styles.link} href={linkZwifterbikes} target="_blank" rel="noreferrer">
+            Выбор велосипеда для маршрута zwifterbikes.web.app
           </a>
         </p>
       )}
