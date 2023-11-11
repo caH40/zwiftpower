33d1ee440dca2c7b3fba910d510fd341096efe30
Alexander Berezhnev <freelancerbav@gmail.com>
2023-11-11 21:29:50 +0300

    v2.38.7 moved all result handlers when updating to one module

---
+++ server/src/service/protocol/handler-protocol.ts
@@ -0,0 +1,74 @@
+import { ZwiftEvent } from '../../Model/ZwiftEvent.js';
+import { addGapStart } from '../../utils/gap.js';
+import { addAgeAndFlag } from './age-and-flag.js';
+import { addWattsPerKg } from '../../utils/watts.js';
+import { addMainProfileZwiftToRaw } from '../profile_additional/main-add-row.js';
+import { setRankResultTotal } from './ranging.js';
+import { saveResults } from './results-save.js';
+import { setUpdatedToEvent } from './event-update.js';
+// фильтры
+import { filterByRankCatchup } from './catchup/results-filter.js';
+import { filterByRankNewbies } from './newbies/results-filter.js';
+import { filterByRankClassicCommon } from './classic_common/results-filter.js';
+
+// types
+import {
+  EventWithSubgroup,
+  HandlerProtocolArg,
+  ResultEventAdditional,
+} from '../../types/types.interface.js';
+
+/**
+ * Формирование финишного протокола в зависимости от typeRaceCustom и сохранение в БД
+ */
+export async function handlerProtocol({
+  eventId,
+  results,
+  typeRaceCustom,
+}: HandlerProtocolArg) {
+  // получение данных Эвента из БД
+  const eventDB: EventWithSubgroup | null = await ZwiftEvent.findOne({
+    _id: eventId,
+  }).populate('eventSubgroups');
+
+  if (!eventDB || !eventDB._id) {
+    throw new Error(`Не найден Event с eventId: ${eventId}`);
+  }
+
+  // добавление стартовых гэпов к результатам
+  const resultsWithStartGap: ResultEventAdditional[] = addGapStart(eventDB, results);
+
+  // добавление данных страны и возраста в результаты райдеров
+  const resultsWithAgeAndFlag = await addAgeAndFlag(eventDB, resultsWithStartGap);
+
+  // добавление данных удельной мощности
+  const resultsWithWPK = addWattsPerKg(resultsWithAgeAndFlag);
+
+  // добавление данных основного профиля Zwift райдера в результат Эвента
+  const resultsWithMainProfiles = await addMainProfileZwiftToRaw(resultsWithWPK);
+
+  //=======================================================
+  // фильтрация категорий, сортировка по финишному времени
+  // дисквалификация райдеров с "Виртуальной мощностью"
+  let resultsSorted = [] as ResultEventAdditional[];
+  switch (typeRaceCustom) {
+    case 'catchUp':
+      resultsSorted = filterByRankCatchup(resultsWithMainProfiles);
+      break;
+    case 'newbies':
+      resultsSorted = filterByRankNewbies(resultsWithMainProfiles);
+      break;
+    default: // для всех остальных обрабатывать как 'classicCommon'
+      resultsSorted = filterByRankClassicCommon(resultsWithMainProfiles);
+  }
+  //=======================================================
+
+  // установка ранкинга райдерам
+  const resultsWithRank = await setRankResultTotal(resultsSorted);
+
+  // сохранение результатов в БД
+  await saveResults(eventId, resultsWithRank);
+
+  // обновление данных Event
+  await setUpdatedToEvent(resultsWithRank, eventId);
+}
