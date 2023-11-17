0cca15e838da07e81418fc72437eb9af397385ab
Alexander Berezhnev <freelancerbav@gmail.com>
2023-11-17 18:37:00 +0300

    v2.40.10 added event organizer - Bike-Caucasus

--- client/src/assets/zwift/organizer.js
+++ client/src/assets/zwift/organizer.js
@@ -1,4 +1,5 @@
 export const organizers = [
   { id: 0, value: 'KOM-on', name: 'KOM-on' },
-  { id: 1, value: 'Dev', name: 'Dev' },
+  { id: 1, value: 'Bike-Caucasus', name: 'BCA' },
+  { id: 2, value: 'Dev', name: 'Dev' },
 ];
