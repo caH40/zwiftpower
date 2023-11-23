5aa5e886bb3e9597a31f569e47fe538323bdfb08
Alexander Berezhnev <freelancerbav@gmail.com>
2023-11-23 16:56:53 +0300

    v2.41.10 added bc club

--- client/src/assets/zwift/organizer.js
+++ client/src/assets/zwift/organizer.js
@@ -1,5 +1,6 @@
 export const organizers = [
   { id: 0, value: 'KOM-on', name: 'KOM-on' },
   { id: 1, value: 'Bike-Caucasus', name: 'BCA' },
-  { id: 2, value: 'Dev', name: 'Dev' },
+  { id: 2, value: 'bc club', name: 'bc club' },
+  { id: 3, value: 'Dev', name: 'Dev' },
 ];
