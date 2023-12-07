3a7d244d6e12c76f0bf773ca07a6732b171b8d86
Alexander Berezhnev <freelancerbav@gmail.com>
2023-12-07 09:01:14 +0300

    v2.43.2 disabling error logging

--- server/src/errors/error.ts
+++ server/src/errors/error.ts
@@ -1,24 +1,26 @@
-import { nodeEnvType } from '../config/environment.js';
-import { logError } from '../logger/logger.js';
-import { ignoreError } from './ignore.js';
-import { parseError } from './parse.js';
+// import { nodeEnvType } from '../config/environment.js';
+// import { logError } from '../logger/logger.js';
+// import { ignoreError } from './ignore.js';
+// import { parseError } from './parse.js';
 
 export const errorHandler = (error: unknown): void => {
-  try {
-    // выход, если ошибка из списка игнорируемых
-    if (ignoreError(error)) {
-      return;
-    }
-    // если разработка, то выводить ошибку в консоль
-    if (nodeEnvType === 'development') {
-      console.log(error); // eslint-disable-line
-    }
+  console.log(error);
 
-    const errorParsed = parseError(error);
+  // try {
+  //   // выход, если ошибка из списка игнорируемых
+  //   if (ignoreError(error)) {
+  //     return;
+  //   }
+  //   // если разработка, то выводить ошибку в консоль
+  //   if (nodeEnvType === 'development') {
+  //     console.log(error); // eslint-disable-line
+  //   }
 
-    // логирование ошибки
-    logError(errorParsed);
-  } catch (error) {
-    console.log(error); // eslint-disable-line
-  }
+  //   const errorParsed = parseError(error);
+
+  //   // логирование ошибки
+  //   logError(errorParsed);
+  // } catch (error) {
+  //   console.log(error); // eslint-disable-line
+  // }
 };
