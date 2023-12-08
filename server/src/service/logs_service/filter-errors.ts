3a58e9d54bf3bd94fa5346d8248f22331c55d642
Alexander Berezhnev <freelancerbav@gmail.com>
2023-12-08 22:24:59 +0300

    v2.44.7 filtering logs errors

--- server/src/service/logs_service/filter-errors.ts
+++ server/src/service/logs_service/filter-errors.ts
@@ -1,9 +1,32 @@
 // types
 import { LogsErrorSchema } from '../../types/model.interface.js';
+import { getTimerLocal } from '../../utils/date-local.js';
 
 /**
  *  Фильтрация найденных логов по ключевому слову search
  */
 export const filterLogsErrors = (logs: LogsErrorSchema[], search: string | undefined) => {
-  return logs;
+  if (!search) {
+    return logs;
+  }
+
+  const logsFiltered = logs.filter((log) => {
+    if (getTimerLocal(log.timestamp, 'DDMMYYHm').includes(search)) {
+      return true;
+    }
+    if (log.type?.toLowerCase()?.includes(search.toLowerCase())) {
+      return true;
+    }
+    if (log.responseData?.toLowerCase()?.includes(search.toLowerCase())) {
+      return true;
+    }
+    if (log.message?.toLowerCase()?.includes(search.toLowerCase())) {
+      return true;
+    }
+    if (log.stack?.toLowerCase()?.includes(search.toLowerCase())) {
+      return true;
+    }
+  });
+
+  return logsFiltered;
 };
