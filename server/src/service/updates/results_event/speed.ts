c50e5c4d28ccdcb5ca4d280e1296eca1b4c43af0
Alexander Berezhnev <freelancerbav@gmail.com>
2024-01-10 21:40:23 +0300

    v2.55.2 bug fixes when calculating the average speed for riders who started with a gap

--- server/src/service/updates/results_event/speed.ts
+++ server/src/service/updates/results_event/speed.ts
@@ -1,3 +1,4 @@
+import { millisecondsInHour } from '../../../assets/date.js';
 import { errorHandler } from '../../../errors/error.js';
 import { EventWithSubgroup, ResultEventAdditional } from '../../../types/types.interface.js';
 
@@ -21,20 +22,11 @@ export async function addSpeed(
 
       continue;
     }
-    //время старта подгруппы Эвента
-    const eventSubgroupStart = subgroup.eventSubgroupStart;
+    // продолжительность заезда
+    const durationInHour = result.activityData.durationInMilliseconds / millisecondsInHour;
+    const segmentDistanceInKilometers = result.activityData.segmentDistanceInMeters / 1000;
 
-    const gap = new Date(eventSubgroupStart).getTime() - new Date(event.eventStart).getTime();
-
-    // чистое время заезда
-    const timeClear = result.activityData.durationInMilliseconds - gap;
-
-    const speed =
-      Math.round(
-        (100 * result.activityData.segmentDistanceInMeters) / 1000 / (timeClear / 3600000)
-      ) / 100;
-
-    result.speed = speed;
+    result.speed = Math.round((100 * segmentDistanceInKilometers) / durationInHour) / 100;
   }
   return results;
 }
