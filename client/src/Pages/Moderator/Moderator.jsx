1cb2172ae4ab50a71f4d2d85456b2d2bc29be278
Alexander Berezhnev <freelancerbav@gmail.com>
2024-02-06 06:50:25 +0300

    v2.66.5 admin panel

--- client/src/Pages/Moderator/Moderator.jsx
+++ client/src/Pages/Moderator/Moderator.jsx
@@ -1,14 +1,16 @@
-import { NavLink, Outlet } from 'react-router-dom';
-import classNames from 'classnames/bind';
+import { Outlet } from 'react-router-dom';
 
 import useTitle from '../../hook/useTitle';
 import IconEventCreate from '../../components/icons/IconEventCreate';
 import IconEventEdit from '../../components/icons/IconEventEdit';
 import IconEventAdd from '../../components/icons/IconEventAdd';
+import NavAdmin from '../../components/UI/NavAdmin/NavAdmin';
 
-import styles from './Moderator.module.css';
-
-const cx = classNames.bind(styles);
+const items = [
+  { to: '/zwift/event/create', title: 'Создание', icon: IconEventCreate },
+  { to: '/zwift/event/add', title: 'Добавление', icon: IconEventAdd },
+  { to: '/zwift/event/edit', title: 'Редактирование', icon: IconEventEdit },
+];
 
 /**
  * Страница модераторов
@@ -17,43 +19,7 @@ function Moderator() {
   useTitle('Управление Эвентами в Zwift');
   return (
     <section>
-      <nav className={cx('wrapper')}>
-        <ul className={cx('list')}>
-          <li>
-            <NavLink to="/zwift/event/create" className={cx('link')}>
-              {({ isActive }) => (
-                <div className={cx('link__box')}>
-                  <IconEventCreate isActive={isActive} />
-                  <span className={cx({ active: isActive })}>Создание</span>
-                </div>
-              )}
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink to="/zwift/event/add" className={cx('link')}>
-              {({ isActive }) => (
-                <div className={cx('link__box')}>
-                  <IconEventAdd isActive={isActive} />
-                  <span className={cx({ active: isActive })}>Добавление</span>
-                </div>
-              )}
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink to="/zwift/event/edit" className={cx('link')}>
-              {({ isActive }) => (
-                <div className={cx('link__box')}>
-                  <IconEventEdit isActive={isActive} />
-                  <span className={cx({ active: isActive })}>Редактирование</span>
-                </div>
-              )}
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-
+      <NavAdmin items={items} />
       <Outlet />
     </section>
   );
---
+++ client/src/components/UI/NavAdmin/NavAdmin.jsx
@@ -0,0 +1,38 @@
+import { NavLink } from 'react-router-dom';
+import classNames from 'classnames/bind';
+
+import styles from './NavAdmin.module.css';
+
+const cx = classNames.bind(styles);
+
+/**
+ * Панель админа/модератора
+ * @param {{ items : {to:string,title:string,icon:React.JSX.Element} }} to path страницы;
+ * @param title название блока в меню;
+ * @param icon иконка для блока меню в JSX;
+ */
+function NavAdmin({ items }) {
+  return (
+    <nav className={cx('wrapper')}>
+      <ul className={cx('list')}>
+        {items.map((item) => {
+          const Icon = item.icon;
+          return (
+            <li key={item.to}>
+              <NavLink to={item.to} className={cx('link')}>
+                {({ isActive }) => (
+                  <div className={cx('link__box')}>
+                    <Icon isActive={isActive} />
+                    <span className={cx({ active: isActive })}>{item.title}</span>
+                  </div>
+                )}
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
+
+export default NavAdmin;
