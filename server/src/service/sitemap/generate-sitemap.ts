dd927ca8544cc8f32f2366601a560affd08da2d5
Alexander Berezhnev <freelancerbav@gmail.com>
2024-10-21 13:43:00 +0300

    v2.83.9 Generating pages with the rider racing score in the sitemap.

--- server/src/service/sitemap/generate-sitemap.ts
+++ server/src/service/sitemap/generate-sitemap.ts
@@ -6,6 +6,7 @@ import { errorHandler } from '../../errors/error.js';
 import { getUrlsResultsDescription } from './results.js';
 import { getUrlsProfileResults } from './profile-results.js';
 import { urlsStatic } from './static.js';
+import { getUrlsProfileRacingScore } from './profile-racingScore.js';
 
 const __dirname = path.resolve();
 
@@ -14,26 +15,31 @@ const __dirname = path.resolve();
  */
 export async function createSitemap() {
   try {
-    const urlsScheduleDescription = await getUrlsScheduleDescription();
-    const urlsResultsDescription = await getUrlsResultsDescription();
-    const urlsProfileResults = await getUrlsProfileResults();
+    const [
+      urlsScheduleDescription,
+      urlsResultsDescription,
+      urlsProfileResults,
+      urlsProfileRacingScore,
+    ] = await Promise.all([
+      getUrlsScheduleDescription(),
+      getUrlsResultsDescription(),
+      getUrlsProfileResults(),
+      getUrlsProfileRacingScore(),
+    ]);
 
     const data = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urlsStatic}
-    ${urlsScheduleDescription}
-    ${urlsResultsDescription}
-    ${urlsProfileResults}
-  </urlset>	`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${urlsStatic}
+  ${urlsScheduleDescription}
+  ${urlsResultsDescription}
+  ${urlsProfileResults}
+  ${urlsProfileRacingScore}
+</urlset>`;
 
-    fs.writeFile(
+    // Обработка ошибок при записи файла
+    await fs.promises.writeFile(
       path.join(__dirname, '..', '..', 'client', 'build', 'sitemap.xml'),
-      data,
-      (err) => {
-        if (err) {
-          throw err;
-        }
-      }
+      data
     );
   } catch (error) {
     errorHandler(error);
