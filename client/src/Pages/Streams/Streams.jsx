8c4584ef276fedb65e15fc038a585268947dc4f5
Alexander Berezhnev <freelancerbav@gmail.com>
2024-11-03 10:43:06 +0300

    v2.85.2 Basic Streams page and routing to the page.

---
+++ client/src/Pages/Streams/Streams.jsx
@@ -0,0 +1,7 @@
+import styles from './Streams.module.css';
+/**
+ * Страница Трансляций (стримов) с звифта пользователей сайта.
+ */
+export default function Streams() {
+  return <div>Streams</div>;
+}
