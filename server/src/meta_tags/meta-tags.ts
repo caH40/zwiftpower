830e79d8bf51d4c486ff6a6bbdc8570b9282a094
Alexander Berezhnev <freelancerbav@gmail.com>
2024-11-04 16:34:27 +0300

    v2.86.5 Added error handling for generating meta tags on the server for pages.

--- server/src/meta_tags/meta-tags.ts
+++ server/src/meta_tags/meta-tags.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import fs from 'fs';
 
 import { getMetaTags } from './handler.js';
+import { errorHandler } from '../errors/error.js';
 
 const __dirname = path.resolve();
 
@@ -11,17 +12,18 @@ const __dirname = path.resolve();
  * возвращает html файл в ut8 кодировке
  */
 export const setMetaTags = async (url: string): Promise<string> => {
-  const { title, canonical, description, image, recommendationsTag } = await getMetaTags(url);
+  try {
+    const { title, canonical, description, image, recommendationsTag } = await getMetaTags(url);
 
-  // путь до index.html в Билде
-  const filePath = path.resolve(__dirname, '..', '..', 'client', 'build', 'index.html');
+    // путь до index.html в Билде
+    const filePath = path.resolve(__dirname, '..', '..', 'client', 'build', 'index.html');
 
-  // получение файла в переменную htmlContent
-  let htmlContent = fs.readFileSync(filePath, 'utf8');
+    // получение файла в переменную htmlContent
+    let htmlContent = fs.readFileSync(filePath, 'utf8');
 
-  htmlContent = htmlContent.replace(
-    /<meta charset="utf-8">/,
-    `<meta charset="utf-8">
+    htmlContent = htmlContent.replace(
+      /<meta charset="utf-8">/,
+      `<meta charset="utf-8">
   <title>${title}</title>
   <meta name="description" content="${description}" data-rh="true" >
   <link rel="canonical" href="${canonical}" data-rh="true" >
@@ -30,7 +32,29 @@ export const setMetaTags = async (url: string): Promise<string> => {
   <meta property="og:url" content="${canonical}" data-rh="true" >
   <meta property="og:image" content="${image}" data-rh="true" >
   <meta property="yandex_recommendations_tag" content="${recommendationsTag}" data-rh="true" >`
-  );
+    );
 
-  return htmlContent;
+    return htmlContent;
+  } catch (error) {
+    errorHandler(error);
+    return htmlError(error, url);
+  }
 };
+
+// При ошибке возвращается html с описанием ошибки.
+function htmlError(error: unknown, url: string) {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Error</title>
+  </head>
+<body>
+  <div id="root"><p>${JSON.stringify(error, null, 2)}</p><p>Запрашиваемый url: ${url}</p></div>
+</body>
+</html>
+`;
+}
