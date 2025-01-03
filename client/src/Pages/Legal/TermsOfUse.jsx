76e01578e68ee723d4f4ab434a7fd22466098baa
Alexander Berezhnev <freelancerbav@gmail.com>
2025-01-03 20:04:01 +0300

    v2.97.2 Added User Agreement page.

--- client/src/Pages/Legal/TermsOfUse.jsx
+++ client/src/Pages/Legal/TermsOfUse.jsx
@@ -1,9 +1,15 @@
+import Document from '../../components/Document/Document.jsx';
+import { termsOfUse } from '../../locales/terms-of-use.json';
+
 import styles from './Legal.module.css';
 
 /**
  * Страница Пользовательского соглашения.
  */
-
 export default function TermsOfUse() {
-  return <div>TermsOfUse</div>;
+  return (
+    <div className={styles.wrapper}>
+      <Document dataJson={termsOfUse} />
+    </div>
+  );
 }
