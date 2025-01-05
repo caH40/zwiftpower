29c4334edaa59e1b63ff077146cacc59d8ef5498
Alexander Berezhnev <freelancerbav@gmail.com>
2025-01-05 09:31:36 +0300

    v2.97.12 Strict categorization ranges for catch-up and tour have been changed.

--- client/src/assets/zwift/accessExpression.js
+++ client/src/assets/zwift/accessExpression.js
@@ -100,29 +100,31 @@ export const accessExpressions = [
   },
   {
     id: 3,
-    name: 'catchUp',
-    label: 'Догонялки. По категориям.',
-    value: `(powerCurves.category == 0 && subgroup.label == 1) ||
-(powerCurves.category != 5 && powerCurves.category >= subgroup.label) ||
-(powerCurves.category == 5 && subgroup.label == 1)`,
-    description: `Группа A: zMAP: ≥5.1W/kg; zFTP: ≥4.2W/kg, zFTP (watts): ≥250W;
-                  Группа B: zMAP: ≥4.1W/kg; zFTP: ≥3.36W/kg, zFTP (watts): ≥200W;
-                  Группа C: zMAP: ≥3.2W/kg; zFTP: ≥2.63W/kg, zFTP (watts): ≥150W;
-                  Группа D: zMAP: <3.2W/kg; zFTP:<2.63W/kg;
-                  Райдер без категории (E) может присоединиться в группу А;`,
+    name: 'komonCatchUp',
+    label: 'KOMon Догонялки',
+    value: `(subgroup.label == 5 && powerCurves.zFTPwkg < 6) ||
+(subgroup.label == 1 && powerCurves.zFTPwkg < 4.6) ||
+(subgroup.label == 2 && powerCurves.zFTPwkg < 4.2) ||
+(subgroup.label == 3 && powerCurves.zFTPwkg < 3.5) ||
+(subgroup.label == 4 && (powerCurves.zFTPwkg < 2.5 || powerCurves.zFTP < 180))`,
+    description: `Группа E: zFTP (W/kg): 4.6 - 6.0;
+                  Группа A: zFTP (W/kg): 4.2 - 4.59;
+                  Группа B: zFTP (W/kg): 3.5 - 4.19;
+                  Группа C: zFTP (W/kg): 2.5 - 3.49;
+                  Группа D: zFTP (W/kg): 1.0 - 2.49 или zFTP (watts): < 180W;`,
     paceValues: {
-      1: { from: 1, to: 6 },
-      2: { from: 1, to: 6 },
-      3: { from: 1, to: 6 },
-      4: { from: 1, to: 6 },
-      5: { from: 1, to: 6 },
+      1: { from: 4.2, to: 4.59 },
+      2: { from: 3.5, to: 4.19 },
+      3: { from: 2.5, to: 3.49 },
+      4: { from: 1.0, to: 2.49 },
+      5: { from: 4.6, to: 6.0 },
     },
     categoryEnforcement: true,
   },
   {
     id: 4,
-    name: 'catchUpNew',
-    label: 'Догонялки. По удельной мощности.',
+    name: 'komonTour',
+    label: 'KOMon Тур',
     value: `(subgroup.label == 5 && powerCurves.zFTPwkg < 6) ||
 (subgroup.label == 1 && powerCurves.zFTPwkg < 4.6) ||
 (subgroup.label == 2 && (powerCurves.zFTPwkg < 4.2 || powerCurves.zFTP < 240)) ||
@@ -139,24 +141,6 @@ export const accessExpressions = [
       3: { from: 2.5, to: 3.49 },
       4: { from: 1.0, to: 2.49 },
       5: { from: 4.6, to: 6.0 },
-    },
-    categoryEnforcement: true,
-  },
-  {
-    id: 5,
-    name: 'newbies',
-    label: 'Новичковая. По категориям.',
-    value: `(powerCurves.category == 3 && subgroup.label == 3) ||
-(powerCurves.category == 4 && (subgroup.label == 4 || subgroup.label == 3)) || 
-(powerCurves.category == 5)`,
-    description: `Группа C: zFTP (W/kg): 2.63 - 3.359, zMAP (W/kg): 3.2 - 4.09 и zFTP (watts): ≥150W;
-                  Группа D: zFTP (W/kg): 1.0 - 2.629, zMAP (W/kg): 1 - 3.19 и zFTP (watts): <150W;`,
-    paceValues: {
-      1: { from: 1, to: 6 },
-      2: { from: 1, to: 6 },
-      3: { from: 2.63, to: 3.35 },
-      4: { from: 1, to: 2.62 },
-      5: { from: 1, to: 6 },
     },
     categoryEnforcement: true,
   },
