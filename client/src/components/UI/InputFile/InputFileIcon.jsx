d71334309cdd906f6339bba8904a3e207dfba531
Alexander Berezhnev <freelancerbav@gmail.com>
2025-01-20 09:57:51 +0300

    v2.99.4 Fixed a bug during client-side build.

--- client/src/components/UI/InputFile/InputFileIcon.jsx
+++ client/src/components/UI/InputFile/InputFileIcon.jsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { useRef } from 'react';
 import cn from 'classnames/bind';
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,16 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
-    port: 80,
-    host: true, // Позволяет прослушивать все интерфейсы (0.0.0.0)
-    https: false, // Установите true, если используете 443 и сертификаты
-  },
-  build: {
-    outDir: './build',
-    chunkSizeWarningLimit: 1000,
-  },
+export default defineConfig(({ mode }) => {
+  const isDevelopment = mode === 'development';
+
+  return {
+    plugins: [react()],
+    server: {
+      open: true,
+      port: 80,
+      host: true, // Позволяет прослушивать все интерфейсы (0.0.0.0)
+      https: false, // Установите true, если используете 443 и сертификаты
+    },
+    build: {
+      outDir: './build',
+      chunkSizeWarningLimit: 1000,
+      sourcemap: isDevelopment, // Включает sourcemaps только для development
+    },
+  };
 });
