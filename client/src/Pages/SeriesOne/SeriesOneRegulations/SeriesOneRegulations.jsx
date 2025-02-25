bb21ed97e2dad24f9bec35c3d440e0b011f75567
Alexander Berezhnev <freelancerbav@gmail.com>
2025-02-25 17:33:55 +0300

    v2.102.8 Added a seroes description page.

--- client/src/Pages/SeriesOne/SeriesOneRegulations/SeriesOneRegulations.jsx
+++ client/src/Pages/SeriesOne/SeriesOneRegulations/SeriesOneRegulations.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 
 import useTitle from '../../../hook/useTitle';
+import { createHtml } from '../../../utils/html';
 
 import styles from './SeriesOneRegulations.module.css';
 
@@ -8,10 +9,48 @@ import styles from './SeriesOneRegulations.module.css';
  * Страница с Регламентами (описание, правила, призы) Серии заездов.
  */
 export default function SeriesOneRegulations() {
-  const { seriesPublicOne, status: statusPublicOne } = useSelector(
-    (state) => state.seriesPublic
-  );
+  const { seriesPublicOne } = useSelector((state) => state.seriesPublic);
 
   useTitle(`Регламент ${seriesPublicOne?.name}` || 'Серия заездов');
-  return seriesPublicOne && <div className={styles.wrapper}>В разработке...</div>;
+  return (
+    seriesPublicOne && (
+      <div className={styles.wrapper}>
+        {seriesPublicOne.description && (
+          <section className={styles.content}>
+            <h3 className={styles.title}>Описание</h3>
+            <p
+              className={styles.paragraph}
+              dangerouslySetInnerHTML={{
+                __html: createHtml.description(seriesPublicOne.description),
+              }}
+            />
+          </section>
+        )}
+
+        {seriesPublicOne.prizes && (
+          <section className={styles.content}>
+            <h3 className={styles.title}>Призы</h3>
+            <p
+              className={styles.paragraph}
+              dangerouslySetInnerHTML={{
+                __html: createHtml.description(seriesPublicOne.prizes),
+              }}
+            />
+          </section>
+        )}
+
+        {seriesPublicOne.rules && (
+          <section className={styles.content}>
+            <h3 className={styles.title}>Правила</h3>
+            <p
+              className={styles.paragraph}
+              dangerouslySetInnerHTML={{
+                __html: createHtml.description(seriesPublicOne.rules),
+              }}
+            />
+          </section>
+        )}
+      </div>
+    )
+  );
 }
