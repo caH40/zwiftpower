8a401e173420cb17e8b7c441275245caf66b4c26
Alexander Berezhnev <freelancerbav@gmail.com>
2025-08-22 16:44:04 +0300

    v.2.111.3 Finance and Services page

--- client/src/Pages/Profile/SettingsServicesAndFinances/SettingsServicesAndFinances.jsx
+++ client/src/Pages/Profile/SettingsServicesAndFinances/SettingsServicesAndFinances.jsx
@@ -8,12 +8,42 @@ import styles from './SettingsServicesAndFinances.module.css';
  * Страница финансов и оплаченных сервисов.
  */
 export default function SettingsServicesAndFinances() {
-  useTitle('Настройка оповещений');
+  useTitle('Финансы и Сервисы');
 
   const { zwiftId: zwiftIdAuth } = useSelector((state) => state.checkAuth.value.user);
   return (
-    <div>
-      {zwiftIdAuth && <div className={styles.wrapper__block}>SettingsServicesAndFinances</div>}
+    <div className={styles.wrapper}>
+      <div>
+        <h3 className={styles.title}>Пополнить или оплатить сервисы</h3>
+        {zwiftIdAuth && (
+          <div className={styles.wrapper__block}>Пополнить или оплатить сервисы</div>
+        )}
+      </div>
+
+      <div>
+        <h3 className={styles.title}>Ваши активные сервисы или Подписки в работе</h3>
+        {zwiftIdAuth && (
+          <div className={styles.wrapper__block}>
+            Ваши активные сервисы или Подписки в работе
+          </div>
+        )}
+      </div>
+
+      <div>
+        <h3 className={styles.title}>Истёкшие или завершённые сервисы</h3>
+        {zwiftIdAuth && (
+          <div className={styles.wrapper__block}>Истёкшие или завершённые сервисы</div>
+        )}
+      </div>
+
+      <div>
+        <h3 className={styles.title}>История транзакций и покупок или Платежи за сервисы</h3>
+        {zwiftIdAuth && (
+          <div className={styles.wrapper__block}>
+            История транзакций и покупок или Платежи за сервисы
+          </div>
+        )}
+      </div>
     </div>
   );
 }
