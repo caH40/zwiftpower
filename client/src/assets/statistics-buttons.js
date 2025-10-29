06d52c234fd38dfbef9f6612c389fa04c4dc8976
Alexander Berezhnev <freelancerbav@gmail.com>
2025-10-29 16:21:38 +0300

    v.2.121.1 Navigation button for the team statistics page.

--- client/src/assets/statistics-buttons.js
+++ client/src/assets/statistics-buttons.js
@@ -1,8 +1,9 @@
 export const statisticsButtons = [
-  { id: 0, page: 'main', name: 'Статистика' },
-  { id: 1, page: 'leaders/male', name: 'Лидеры' },
-  { id: 2, page: 'riders-ftp', name: 'FTP' },
-  { id: 3, page: 'riders-racing-score', name: 'Racing Score' },
+  { id: 0, page: 'main', name: 'Общие' },
+  { id: 1, page: 'teams', name: 'Команды' },
+  { id: 2, page: 'leaders/male', name: 'Лидеры' },
+  { id: 3, page: 'riders-ftp', name: 'FTP' },
+  { id: 4, page: 'riders-racing-score', name: 'RS' },
 ];
 export const LeadersGenderButtons = [
   { id: 0, page: 'male', name: 'Мужчины' },
