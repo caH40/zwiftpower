4983987b12eaf4337e92c03ea8de6a8e78d17b6d
Alexander Berezhnev <freelancerbav@gmail.com>
2026-01-23 08:20:23 +0300

    v.2.125.71 Fixed a bug during manual updating of the event protocol.

--- server/src/service/updates/results_event/criticalpower/fitfile-save.ts
+++ server/src/service/updates/results_event/criticalpower/fitfile-save.ts
@@ -8,7 +8,6 @@ import { FitFileToDBParams } from '../../../../types/types.interface.js';
  * @param param0
  */
 export async function saveFitFile({ powerInWatts, result, nameAndDate }: FitFileToDBParams) {
-  // Объект активности, который хотим добавить в activities
   const power = {
     name: nameAndDate.name,
     date: nameAndDate.eventStart,
@@ -16,54 +15,16 @@ export async function saveFitFile({ powerInWatts, result, nameAndDate }: FitFile
     powerInWatts: JSON.stringify(powerInWatts),
     weightInGrams: result.profileData.weightInGrams,
   };
-
-  // Уникальный идентификатор райдера (unique index в коллекции)
   const zwiftId = result.profileId;
+  const fitFileDB = await FitFile.findOne({ zwiftId });
 
-  await FitFile.findOneAndUpdate(
-    // Фильтр ТОЛЬКО по zwiftId.
-    // Никаких условий по activities — иначе upsert может создать дубликат документа.
-    { zwiftId },
+  if (!fitFileDB) {
+    await FitFile.create({ zwiftId });
+  }
 
-    // Update pipeline — позволяет использовать агрегационные операторы.
-    [
-      {
-        $set: {
-          activities: {
-            // Проверяем: есть ли уже activity с таким eventId.
-            $cond: [
-              // $in ищет eventId внутри массива activities.eventId.
-              { $in: [nameAndDate.eventId, '$activities.eventId'] },
+  if (fitFileDB?.activities.some((act) => act.eventId === nameAndDate.eventId)) {
+    return;
+  }
 
-              // Если eventId уже есть — оставляем массив без изменений.
-              '$activities',
-
-              // Если eventId нет — добавляем новую activity в конец массива.
-              { $concatArrays: ['$activities', [power]] },
-            ],
-          },
-        },
-      },
-    ],
-
-    { upsert: true }
-  );
+  await FitFile.findOneAndUpdate({ zwiftId }, { $addToSet: { activities: power } });
 }
-
-// export async function saveFitFile({ powerInWatts, result, nameAndDate }: FitFileToDBParams) {
-//   const power = {
-//     name: nameAndDate.name,
-//     date: nameAndDate.eventStart,
-//     eventId: nameAndDate.eventId,
-//     powerInWatts: JSON.stringify(powerInWatts),
-//     weightInGrams: result.profileData.weightInGrams,
-//   };
-//   const zwiftId = result.profileId;
-//   const fitFileDB = await FitFile.findOne({ zwiftId });
-
-//   if (!fitFileDB) {
-//     await FitFile.create({ zwiftId });
-//   }
-
-//   await FitFile.findOneAndUpdate({ zwiftId }, { $addToSet: { activities: power } });
-// }
