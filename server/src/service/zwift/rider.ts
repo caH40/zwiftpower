84fc13a45ceaaaab445a4f747363e10be4dad798
Alexander Berezhnev <freelancerbav@gmail.com>
2026-03-06 09:33:03 +0300

    v2.126.16 A bug has been fixed when updating race results in which a rider with a non-existent zwiftId in the database participated.

--- server/src/service/zwift/rider.ts
+++ server/src/service/zwift/rider.ts
@@ -1,6 +1,8 @@
+import { handleAndLogError } from '../../errors/error.js';
 import { ProfileZwiftAPI } from '../../types/zwiftAPI/profileFromZwift.interface.js';
 import { getRequest } from './api/request-get.js';
 import { addInvalidZwiftId } from './invalid_zwift_id/add.js';
+import { checkZwiftId } from './invalid_zwift_id/check.js';
 
 /**
  *  запрос данных Райдера с сервера Zwift
@@ -17,8 +19,20 @@ export async function getZwiftRiderService(zwiftId: number): Promise<ProfileZwif
   // Если профиль не найден, то добавляем его в список невалидных zwiftId,
   // для исключения ошибочных запросов на api
   if (!riderData) {
+    const zwiftIdExisted = await checkZwiftId(zwiftId);
+
+    if (zwiftIdExisted) {
+      handleAndLogError(
+        new Error(
+          `Не найден профиль на сервере Zwift. ZwiftId:${zwiftId} уже добавлен в список InvalidZwiftId.`
+        )
+      );
+      return null;
+    }
+
     await addInvalidZwiftId(zwiftId);
-    throw new Error('Не найден профиль на сервере Zwift');
+    handleAndLogError(new Error('Не найден профиль на сервере Zwift'));
+    return null;
   }
 
   return riderData;
